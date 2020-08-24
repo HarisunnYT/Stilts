9c749b0609faa6e173cce7576b8af3ffdeb94608
Unknown <harisunnyt@gmail.com>
2020-08-24 09:57:22 +0930

    Added better victory

--- Assets/_Game/Scripts/UI/VictoryPanel.cs
+++ Assets/_Game/Scripts/UI/VictoryPanel.cs
@@ -11,6 +11,9 @@ public class VictoryPanel : Panel
     [SerializeField]
     private TMP_Text timeText;
 
+    [SerializeField]
+    private GameObject backButton; 
+
     public override void Initialise()
     {
         CompleteTrigger.Instance.OnComplete += ShowPanel;
@@ -22,37 +25,60 @@ public class VictoryPanel : Panel
             CompleteTrigger.Instance.OnComplete -= ShowPanel;
     }
 
-    protected override void OnShow()
+    public void ShowTime()
     {
-        Cursor.visible = true;
+        StartCoroutine(ShowTimeIE());
+    }
 
+    private IEnumerator ShowTimeIE()
+    {
         float completeTime = SaveManager.Instance.GetTimePlayed() + MovementController.Instance.TimePlayed;
 
-        TimeSpan time = TimeSpan.FromSeconds(completeTime);
+        float increment = completeTime / 100;
+        float counter = 0;
+
+        TimeSpan time;
         StringBuilder str = new StringBuilder();
 
-        if (time.Hours > 0)
+        while (counter < completeTime)
         {
-            str.Append(time.Hours);
-            str.Append("h ");
+            str.Clear();
+            counter += increment;
+
+            time = TimeSpan.FromSeconds(counter);
+
+            if (time.Hours > 0)
+            {
+                str.Append(time.Hours);
+                str.Append("h ");
+            }
+
+            if (time.Minutes > 0)
+            {
+                str.Append(time.Minutes);
+                str.Append("m ");
+            }
+
+            str.Append(time.Seconds);
+            str.Append("s");
+
+            timeText.text = str.ToString();
+
+            yield return new WaitForSeconds(0.01f);
         }
 
-        if (time.Minutes > 0)
-        {
-            str.Append(time.Minutes);
-            str.Append("m ");
-        }
-
-        str.Append(time.Seconds);
-        str.Append("s");
-
-        timeText.text = str.ToString();
-
-        SaveManager.Instance.ClearSavedData(SaveManager.Instance.CurrentMap);
+        Cursor.visible = true;
+        backButton.SetActive(true);
     }
 
     public void Back()
     {
+        SaveManager.Instance.ClearSavedData(SaveManager.Instance.CurrentMap);
         SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveManager.Instance.ClearSavedData(SaveManager.Instance.CurrentMap);
     }
 }
